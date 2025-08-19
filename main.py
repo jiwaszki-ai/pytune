983c152d4b20864ac8a343586591810716534b56
jiwaszki <jiwaszkiewicz6@gmail.com>
2025-08-20 00:11:14 +0200

    Worksave

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 
     # Initialize pygame display to handle events
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption("PyTune 0.0.0dev")
+    pygame.display.set_caption("PyTune 0.0.1dev")
 
     folder_path = "./assets/domki2024"  # Change this to your folder path
     game = Game(path=folder_path, random_order=True)
